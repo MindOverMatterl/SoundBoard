528f6ce398198cdebcfd6798a7603c875e60aa34
MindOverMatterl <albert.vargas@tecsup.edu.pe>
2024-10-09 20:38:08 -0500

    Lab 10 Terminado

--- VargasASoundBoard2/ViewController.swift
+++ VargasASoundBoard2/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+   
+    var reproducirAudio:AVAudioPlayer?
+    
+    @IBOutlet weak var tablaGrabacione: UITableView!
+    var grabaciones:[Grabacion] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tablaGrabacione.dataSource = self
+        tablaGrabacione.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do{
+            grabaciones = try
+            context.fetch(Grabacion.fetchRequest())
+            tablaGrabacione.reloadData()
+        }catch{}
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return grabaciones.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let grabacion = grabaciones[indexPath.row]
+        cell.textLabel?.text = grabacion.nombre
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let grabacion = grabaciones[indexPath.row]
+        do{
+            reproducirAudio = try AVAudioPlayer(data: grabacion.audio! as Data)
+            reproducirAudio?.play()
+        }catch{}
+        
+        tablaGrabacione.deselectRow(at: indexPath, animated: true)
+        
     }
 
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let grabacion = grabaciones[indexPath.row]
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(grabacion)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            do{
+                grabaciones = try
+                context.fetch(Grabacion.fetchRequest())
+                tablaGrabacione.reloadData()
+            }catch{}
+        }
+    }
+    
+    
 
 }
 
